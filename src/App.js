3991632f5f4cc0b98ac200fbae25f8f01e6459e7
Naimish <naimishbbhuva@gmail.com>
2022-08-19 17:35:58 +0530

    header done, front page design pretty much done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import HomePage from "./pages/Homepage";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Header />
+            <div className="main">
+                <HomePage />
+            </div>
+        </div>
+    );
 }
 
 export default App;
